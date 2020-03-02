975b1c69074e28424bca139a84fe5031ba05a075
ithianand9523 <prakruthianand9523@gmail.com>
2020-03-02 12:15:58 +0530

    second commit

---
+++ demoA2.java
@@ -0,0 +1,38 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class demoA2 {
+	static
+	{
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	}
+
+	public static void main(String[] args) {
+		//open the browser
+	    WebDriver  driver =new ChromeDriver();
+	    
+	    //maximize the browser
+	    driver.manage().window().maximize();
+	    
+	    //enter the URL or path of html file
+	    driver.get("file:///C:/Users/Prathika/Desktop/link.html");
+	    
+	    //String tagname="a";
+	    
+	   // By by = By.tagName(tagname);
+	    
+	    //WebElement link = driver.findElement(by);
+	    //link.click();
+	    
+	    //or
+	    
+	    WebElement link = driver.findElement(By.tagName("a"));
+	    link.click();
+	   
+	}
+
+}
